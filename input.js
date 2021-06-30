60dce791fdfe65dcec3fa787c49c420f16fea035
shivanix <shvni.here@gmail.com>
2021-06-30 22:49:29 +0000

    implemented special key to send messages to server

--- input.js
+++ input.js
@@ -15,6 +15,12 @@ const handleUserInput = function(key) {
   } else if (key === "d") {
     connection.write("Move: right");
   }
+  if(key === "r"){
+    let bubbleId = Math.floor(Math.random() * 5);
+    const phrases = ["Say: Whatevvverr", "Say: I do what I want", "Say: Lombite", "Say: Foodie","Say: Gororoba" ]; 
+
+      connection.write(phrases[bubbleId]);
+  }
 };
 
 const setupInput = function(conn) {

9ea0cd4124702790c4b21e8a6147db4682037c38
shivanix <shvni.here@gmail.com>
2021-06-30 22:24:33 +0000

    bound specific keyboard keys to the movement commands expected by the server

--- input.js
+++ input.js
@@ -1,27 +1,35 @@
+let connection;
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   // your code here
   if (key === '\u0003') {
     process.exit();
   }
   key = key.toLowerCase();
   if (key === 'w') {
-    console.log("Up");
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", (key) => {
-    handleUserInput(key)
+    handleUserInput(key);
   });
 
   return stdin;
 };
 
 module.exports = {
-setupInput
-}
+  setupInput
+};

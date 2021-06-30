70441d4b20bfa3f1cd0d14b45e555c0899f751ca
shivanix <shvni.here@gmail.com>
2021-06-30 21:34:25 +0000

    registered an event listener for stdin, & started implemtng the handlerUserInput f

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 
 // establishes a connection with the game server
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133',
     port: 50542
@@ -11,20 +11,23 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("data", function(message) {
+  conn.on("data", function (message) {
     console.log(`server has said: ${message}`);
   });
 
-  conn.on("connect", function(){
+  conn.on("connect1", function () {
     console.log(`Successfully connected to game server`);
   })
-    conn.on('connect', () => {
-      conn.write("Name: GGG");
-  
-})
-  return conn; 
+  conn.on('connect2', () => {
+    conn.write("Name: GGG");
+
+  })
+
+  return conn;
+
 };
 
-module.exports = {connect
+module.exports = {
+  connect
 
 };
--- play.js
+++ play.js
@@ -1,4 +1,32 @@
-const {connect} = require("./client")
+const {
+  connect
+} = require("./client")
+
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+  key = key.toLowerCase();
+  if (key === 'w') {
+    console.log("Up");
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (key) => {
+    handleUserInput(key)
+  });
+
+  return stdin;
+};
+
 
 console.log("Connecting ...");
 connect();
+setupInput();
